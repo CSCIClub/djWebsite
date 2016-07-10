3983c14de3bafc7fa7f2e2f78df0de45ea55ee76
wes <carlsonwes@gmail.com>
2016-07-09 21:04:34 -0500

    added news template

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+import news.views.index
 
 # Create your views here.
 def index(request):
-
 	context = {
 		# 'latest_article_list': latest_article_list,
 	}
 	return render(request, 'homepage/index.html', context)
+
+def index2(request):
+    context = {
+            news = news.views.index(request)
+    }
+    return render(request, 'homepage/index2.html', context)
