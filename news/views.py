7b39ba293f27584907f55fa90e1ff334afc5beb9
wes <carlsonwes@gmail.com>
2016-08-09 19:40:35 -0500

    same as last commit :^)

--- news/views.py
+++ news/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
+from django.http import HttpResponse
 
 from .models import Article
 
 # Create your views here.
 def index(request):
-	latest_article_list = Article.objects.order_by('-pub_date')[:5]
-	context = {
-		'latest_article_list': latest_article_list,
-	}
-	return render(request, 'news/index.html', context)
+	return render(request, 'news/index.html', {})
 
 def detail(request, article_id):
 	article = get_object_or_404(Article, pk=article_id)
