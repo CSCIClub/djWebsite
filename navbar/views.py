6d21e937ed02c7f4be230257aa22870fa01c3167
AndrewMorris-scsu <moan1303@stcloudstate.edu>
2016-08-11 22:48:45 -0500

    Created and implemented the navbar customtag

--- navbar/views.py
+++ navbar/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+
+from .models import Article
 
 # Create your views here.
+def index(request):
+	return render(request, 'navbar/index.html', {})
+
+def navbarRender(request, article_id):
+	return render(request, 'navbar/navbar.html', {})
