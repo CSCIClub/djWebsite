7012b70fd959e92dcf7a56525de95f0d1b13e8ed
Jeffrey Witthuhn <jeffdwitt@gmail.com>
2016-08-26 20:27:00 -0500

    added a hard coded shity calendar, need to design in such a way that it takes entries in database and loads them in.

--- djWebsite/urls.py
+++ djWebsite/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('homepage.urls')),
+    url(r'^Calendar/', include('Calendar.urls')),
     url(r'^news/', include('news.urls', namespace='news')),
 ]
