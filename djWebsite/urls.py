1fd3008cf233ab9cee35e4bf4b98251b95ad58c4
AndrewMorris-scsu <moan1303@stcloudstate.edu>
2016-08-29 20:14:06 -0500

    Revert "Revert "wiki working""
    This reverts commit 38c546eaafd7c89d8268be0c9f25d3a27a124374.

--- djWebsite/urls.py
+++ djWebsite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from wiki.urls import get_pattern as get_wiki_pattern
+from django_nyt.urls import get_pattern as get_nyt_patern
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('homepage.urls')),
