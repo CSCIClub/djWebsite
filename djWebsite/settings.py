bf56fcbf20fe5330192f3791c10a10ea73a755b8
wes <carlsonwes@gmail.com>
2016-09-04 13:02:52 -0500

    wiki homepage navbar link

--- djWebsite/settings.py
+++ djWebsite/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'djWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['wiki/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
