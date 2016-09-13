f2698c888f4027934e307b188d346cfc9fa33bd4
wes <carlsonwes@gmail.com>
2016-09-12 20:38:12 -0500

    more bug fixes

--- events/templatetags/events_extras.py
+++ events/templatetags/events_extras.py
@@ -8,8 +8,10 @@ register = template.Library()
 
 @register.inclusion_tag('events/mtng.html')
 def get_next_meeting():
-	upcoming_meeting = [d for d in Event.objects.order_by('-date') if
-            (d.end_date and d.end_date >= date.today() ) or
-            d.date >= date.today() and d.event_type == 'MEETING'][::-1][0]
+    meetings = [d for d in Event.objects.order_by('-date') if
+            ((d.end_date and d.end_date >= date.today() ) or
+            d.date >= date.today()) and d.event_type == 'MEETING'][::-1]
 
-	return {'upcoming_meeting' : upcoming_meeting}
+    upcoming_meeting = meetings[0] if len(meetings) > 0 else None
+
+    return {'upcoming_meeting' : upcoming_meeting}
