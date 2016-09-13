550df20a4410d25b6fdd8816c9da3b7a13bc46ac
Jeffrey Witthuhn <jeffdwitt@gmail.com>
2016-09-12 19:33:25 -0500

    YOLO NO TEST SHIP: enable filtering of event list by meeting, competitions, and hackathons

--- events/urls.py
+++ events/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'events'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^competitions/', views.index, name='index', {'evt_type':'CODECOMP'}),
+	url(r'^meetings/', views.index, name='index', {'evt_type':'MEETING'}),	
+	url(r'^hackathons/', views.index, name='index'{'evt_type':'HACKATHON'}),	
 	url(r'^(?P<event_id>[0-9]+)/$', views.detail, name='detail')
 ]
