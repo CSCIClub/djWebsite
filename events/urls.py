efd63b66cc897723a80fa8e3bad02355081a167f
wes <carlsonwes@gmail.com>
2016-09-15 19:12:10 -0500

    fixed navbar events url

--- events/urls.py
+++ events/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 app_name = 'events'
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
 	url(r'^competitions/', views.index, {'evt_type':'CODECOMP'}, name='index'),
 	url(r'^meetings/', views.index, {'evt_type':'MEETING'}, name='index'),
 	url(r'^hackathons/', views.index, {'evt_type':'HACKATHON'}, name='index'),
+	url(r'^$', views.index, name='index'),
+    #I changed the order because apparently {{url 'events:index'}} will resolve to the last one of
+    #these listed, which seems kind of dumb so we should fix that eventually but for now this works
+
 	url(r'^(?P<event_id>[0-9]+)/$', views.detail, name='detail')
 ]
