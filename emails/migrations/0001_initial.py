0439b07ffbaf171bb07ac65a03fa6db7dad074b3
Jared Rickert <jaredrickert52@gmail.com>
2017-02-04 18:47:01 -0600

    fixed duplicate emails

--- emails/migrations/0001_initial.py
+++ emails/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             name='Email',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('address', models.EmailField(max_length=254)),
+                ('address', models.EmailField(max_length=254, unique=True)),
             ],
         ),
     ]
--- emails/views.py
+++ emails/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.db import IntegrityError
 
 from .forms import EmailForm
 from .models import Email
+
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         form = EmailForm(request.POST)
         if form.is_valid():
-            e = Email(address = form.cleaned_data['email_address'])
-            e.save()
-            return  HttpResponseRedirect('success/')
+            try:
+                e = Email(address=form.cleaned_data['email_address'])
+                e.save()
+            except IntegrityError:
+                pass
+            return HttpResponseRedirect('success/')
     else:
         form = EmailForm()
 
     return render(request, 'emails/index.html', {'form': form})
+
 
 def success(request):
     return render(request, 'emails/success.html', {})
