0575f7ac81dd58ed57d33727519e6eea46b149fd
Jared Rickert <jaredrickert52@gmail.com>
2017-02-04 21:42:56 -0600

    made email model address to be unique

--- emails/models.py
+++ emails/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Email(models.Model):
-    address = models.EmailField()
+    address = models.EmailField(unique=True)
     def __str__(self):
         return self.address
